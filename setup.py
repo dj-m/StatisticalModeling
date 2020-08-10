192bdb7023a70463a35195d057fb05c111b62c55
dj-m <domingomoronta@gmail.com>
2020-08-10 11:44:05 -0700

    Created data science cookiecutter structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Implementation of linear regression in Python to model alcoholic level in wines.',
+    author='Domingo Moronta',
+    license='',
+)
